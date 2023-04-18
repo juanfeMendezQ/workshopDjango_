bf89f0df3bafacc3c549617835e0cfa1e7e0c774
juanfeMendezQ <125328759+juanfeMendezQ@users.noreply.github.com>
2023-04-17 20:51:34 -0500

    pag 45_error

--- appProductos/urls.py
+++ appProductos/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('categorias/', views.verCategorias, name='categorias'), 
     path('productos/<str:idCategoria>', views.verProductos, name='productos'),
     path('producto/<str:idProd>',views.verProductos, name='un_procducto'),
+    path('carro/<str:idProd>', views.agregarCarro, name='agregarCarro'), 
+    path('carrito/', views.verCarrito, name='carrito'),
+    path('eliminar/<str:id>', views.eliminarCarrito, name='eliminar'),
+    path('cambiarCantidad/', views.cambiarCantidad),
 ]
--- appProductos/views.py
+++ appProductos/views.py
@@ -38,3 +38,31 @@ def regProducto(request, idProd, msj = None):
         context['mensaje']= msj
     #renderizar
     return render(request, 'productos/productos.html', context)
+def agregarCarro(request, idProd):
+    idProd = int(idProd) 
+    regUsuario = request.user 
+    msj = None
+    #leer reg del producto en Producto
+    existe = Producto.objects.filter(id=idProd).exists() 
+    if existe:
+        regProducto = Producto.objects.get(id=idProd)
+
+
+        # si no existe en carrito:
+        existe = Carro.objects.filter(producto=regProducto, estado= 'activo', usuario= regUsuario).exists()
+if existe:
+# instanciar un objeto de la clase Carrito
+regCarro = Carro.objects.get(producto=regProducto, estado= 'activo', usuario= regUsuario) #incrementar cantidad
+regCarro.cantidad += 1 
+else:
+regCarro = Carro(producto=regProducto, usuario= regUsuario, valUnit = regProducto.precioUnitario)
+
+# guardar el registro regCarro.save()
+
+else:
+# dar mensaje
+msj = 'Producto no disponible'
+
+
+# redireccionar a 'verProducto' return verProducto(request, idProd, msj)
+
